5bc337447c6393e3debcdf859b910bea840ac840
Tam Vu <vutam_a@hotmail.com>
2019-12-27 16:46:36 +0000

    add gifts/new routes and view

--- app/controllers/gift_controller.rb
+++ app/controllers/gift_controller.rb
@@ -6,6 +6,22 @@ class GiftController < ApplicationController
     erb :'gifts/index'
   end
 
+  get '/gifts/new' do
+    erb :'/gifts/new'
+  end
+
+  post '/gifts' do
+    #@gift = Gift.create(name: params[:name], giver: params[:giver], category: params[:category], description: params[:description], occasion_id: current_user.id)
+    if !params[:name].empty?
+      @gift = Gift.create(params)
+      @gift.save
+      redirect "/gifts/#{@gift.id}"
+    else
+      redirect "/occasions/new"
+    end
+
+  end
+
 
   get '/gifts/:id' do
     @gift = Gift.find(params[:id])
